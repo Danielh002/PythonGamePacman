28b31a3b56d610aba1600ae9b19aa95047381020
Andres Lenis <lenisandres@hotmail.com>
2017-11-24 21:25:50 -0500

    power up agregado

--- segundoNivel/Game.py
+++ segundoNivel/Game.py
@@ -18,13 +18,29 @@ class Game:
         self.background = self.background.convert()
         self.background.fill((0,0,0))
         self.flocking=flocking.flocking([self.ghost]+self.litleGhost)
+        self.timePWUP=0
+        self.timeInitPWUP=100
+        self.timeFinishiPWUP=150
+        self.powerUp=pygame.sprite.Sprite()
+        self.powerUp.image=self.powerUpImage
+        self.powerUp.rect=self.powerUpImage.get_rect()
         
+    def addLittleGhost(self):
+        self.litleGhost.append(LitleGhost(100,500))
+        self.flocking=flocking.flocking([self.ghost]+self.litleGhost)
+
+    def removeLittleGhost(self):
+        print(len(self.litleGhost))
+        obj=self.litleGhost.pop()
+        del(obj)
+        self.flocking=flocking.flocking([self.ghost]+self.litleGhost)
 
     def loadImages(self):
         self.wallImage=pygame.image.load("images/wall.png")
         self.playerImage=pygame.image.load("images/snake.png")
         self.obstaculo1Image=pygame.image.load("images/obstaculo1.png")
         self.obstaculo2Image=pygame.image.load("images/obstaculo2.png")
+        self.powerUpImage=pygame.image.load("images/PWUP.png")
         
     def start(self):
         while(not self.gameOver):
@@ -47,9 +63,22 @@ class Game:
             for i in self.litleGhost:
                 i.move(self.ghost.rect.left,self.ghost.rect.top)
             self.clock.tick(25)
+            if(self.timePWUP==self.timeInitPWUP):
+                self.powerUp.rect.top=100;self.powerUp.rect.left=500
+            if(self.timePWUP>=self.timeFinishiPWUP):
+                self.timePWUP=0
             self.window.blit(self.background,(0,0))
+            if(self.timePWUP>=self.timeInitPWUP):
+                self.window.blit(self.powerUp.image,self.powerUp.rect)
+            if(self.player.rect.colliderect(self.powerUp.rect)):
+                self.timePWUP=0;self.powerUp.rect.top=-100;self.powerUp.rect.left=-100
+                self.removeLittleGhost()
+            elif(self.ghost.rect.colliderect(self.powerUp.rect)):
+                self.timePWUP=0;self.powerUp.rect.top=-100;self.rect.left=-100
+                self.addLittleGhost()
             self.window.blit(self.player.image,self.player.rect)
             self.window.blit(self.ghost.image,self.ghost.rect)
             for i in self.litleGhost:
                 self.window.blit(i.image,i.rect)
             pygame.display.update()
+            self.timePWUP+=1
